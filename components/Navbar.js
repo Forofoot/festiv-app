086debd9cde5e41d71d18cb779f9f9775af79f60
Hubert <erwan.marques@yahoo.fr>
2022-07-21 22:07:56 +0200

    Login

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,52 @@
 
+import Link from "next/link"
+import { useCookies } from "react-cookie"
+import { useState, useEffect } from "react"
+import toast, { Toaster } from 'react-hot-toast';
+
 export default function Navbar() {
+
+  const [cookies, setCookie, removeCookie] = useCookies()
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    setCurrentUser(cookies.user)
+  }, [cookies.user])
+  
+  const logout = (e) =>{
+    e.preventDefault()
+    setCurrentUser(null)
+    removeCookie('user',  {path: '/'})
+    toast.success('Déconnecté')
+  }
   return (
     <header>
-      <h2>Ceci est un header</h2>
+      <Toaster/>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/auth/signup'>
+              <a>
+                S&apos;inscrire
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/auth/signin'>
+              <a>
+                Se connecter
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <button onClick={(e) => logout(e)}>
+              Se déconnecter
+            </button>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
---
+++ pages/auth/signup.js
@@ -0,0 +1,79 @@
+import { useState } from "react"
+import toast, { Toaster } from 'react-hot-toast';
+import { useCookies } from "react-cookie";
+import { useRouter } from "next/router";
+
+export default function Signup () {
+    const [inputedUser, setInputedUser] = useState({
+        email: "",
+        firstName: "",
+        lastName: "",
+        password: ""
+    })
+
+    const router = useRouter()
+    
+    const [cookies, setCookie, removeCookie] = useCookies()
+
+    const handleCreateUser = async (e) =>{
+        e.preventDefault()
+        try{
+            toast.loading('Inscription en cours...')
+            if (!inputedUser.email || !inputedUser.email.includes('@') || !inputedUser.password || !inputedUser.firstName || !inputedUser.lastName) {
+                toast.remove()
+                toast.error('Informations incorrectes')
+            }else{
+                //POST form values
+                const res = await fetch('/api/auth/signup', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        email: inputedUser.email,
+                        firstName: inputedUser.firstName,
+                        lastName: inputedUser.lastName,
+                        password: inputedUser.password,
+                    }),
+                });
+                
+                const data = await res.json();
+
+                if(res.ok){
+                    toast.remove()
+                    toast.success('Compte créé')
+                    setCookie("user", JSON.stringify(data), {
+                        path: '/',
+                        maxAge: 3600, // Expires after 1hr
+                        sameSite: true,
+                    })
+                    router.push('/')
+                    toast.success('Connecté')
+                }else{
+                    toast.remove()
+                    toast.error('Erreur lors de la création du compte')
+                }
+            }
+        }catch(error){
+            console.log(error)
+        }
+    }
+    return(
+        <>
+            <Toaster/>
+            <h1>S&apos;inscrire</h1>
+
+            <form method='POST' onSubmit={handleCreateUser}>
+                <label>Email</label>
+                <input type="text" value={inputedUser.email || ""} placeholder='email' onChange={(e) => setInputedUser({ ...inputedUser, email:e.target.value })}/>
+                <label>Prénom</label>
+                <input type="text" value={inputedUser.firstName || ""} placeholder='Prénom' onChange={(e) => setInputedUser({ ...inputedUser, firstName:e.target.value })}/>
+                <label>Nom</label>
+                <input type="text" value={inputedUser.lastName || ""} placeholder='Nom' onChange={(e) => setInputedUser({ ...inputedUser, lastName:e.target.value })}/>
+                <label>Mot de passe</label>
+                <input type="password"  value={inputedUser.password || ""} placeholder='Mot de passe' minLength={8} onChange={(e) => setInputedUser({ ...inputedUser, password:e.target.value })}/>
+                <button type='submit' className="btnPrimary btnMore hoverEffect">S&apos;inscrire</button>
+            </form>
+        </>
+    )
+}
