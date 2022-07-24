4946c3c36a816985de25ebb1e4e0ae7e89fab2d3
Hubert <erwan.marques@yahoo.fr>
2022-07-24 23:47:11 +0200

    Cloudinary settings

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -10,7 +10,7 @@ export function uploadAvatar(imageUploaded) {
   return new Promise((resolve, reject) => {
     cloudinary.uploader.upload(
       imageUploaded,
-      { crop: "fill", folder: 'avatar' },
+      { crop: "fill", folder: 'avatar', upload_preset: 'my-uploads', width: 368, height: 368 },
       (err, res) => {
         if (err) reject(err);
         resolve(res);
