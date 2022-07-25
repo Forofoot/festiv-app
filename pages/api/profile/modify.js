008dea955a4b62cfaa99bd9c9760e2968bae4fdb
Hubert <erwan.marques@yahoo.fr>
2022-07-25 23:23:56 +0200

    erreur

--- pages/api/profile/modify.js
+++ pages/api/profile/modify.js
@@ -25,7 +25,6 @@ export default async function handler(
             form.parse(req, function (err, fields, files) {
                 if (err) return reject(err);
                 resolve({ fields, files });
-                res.status(200).json({message: 'Donnée récupérée'})
             });
         });
         
