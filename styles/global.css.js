5bdca763eece2041a3567131049c4d17f784abcb
Hubert <erwan.marques@yahoo.fr>
2022-07-31 16:55:59 +0200

    Navbar

--- styles/global.css.js
+++ styles/global.css.js
@@ -15,15 +15,20 @@ export default createGlobalStyle`
     scroll-behavior: smooth;
   }
   body {
-    margin: 0;
+    margin: 0; 
     padding: 0;
     min-height:100vh;
     width: 100%;
     font-family: 'Roboto', sans-serif;
-    font-size: 16px;
+    font-size: 12px;
     background-color: #EFEFEF;
+    &::-webkit-scrollbar{
+      display: none;
+    }
+    -ms-overflow-style: none;
+    scrollbar-width: none;
     @media (min-width: 768px) {
-      font-size: 20px;
+      font-size: 16px;
     }
   }
 
@@ -37,6 +42,7 @@ export default createGlobalStyle`
   a{
     cursor: pointer;
     text-decoration: none;
+    color: inherit;
   }
 
   form{
@@ -46,8 +52,52 @@ export default createGlobalStyle`
     gap: 15px;
   }
 
+  .btnPrimary{
+    all: unset;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 0px;
+    width: 210px;
+    color: var(--white);
+    background: #ffffff;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    transition: all .3s ease;
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      background: linear-gradient(90.32deg, #6300B1 8.05%, #002762 82.81%);
+      transition: transform 0.3s cubic-bezier(0.7, 0, 0.2, 1);
+      height: 100%;
+    }
+    span{
+      position: relative;
+      color: var(--white);
+      font-weight: 400;
+    }
+    &:hover{
+      &::before {
+        transform: translate3d(0,-100%,0);
+      }
+      span{
+        color: var(--secondary);
+      }
+    }
+    @media ${device.mobile}{
+      width: 175px;
+    }
+  }
+
   :root{
     --primary: #000000;
     --secondary: #7620BB;
+    --white: #ffffff;
   }
 `
